26a2f463da1963e53a00d7632dce18df4afcec0c
Maki Tolentino <maki.tolentino.03@gmail.com>
2019-10-12 07:38:44 +0800

    Initial working prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text("Magic 8 Ball"),
+            backgroundColor: Colors.blue,
+          ),
+          body: Magic8Ball(),
+        ),
       ),
     );
+
+class Magic8Ball extends StatefulWidget {
+  @override
+  _Magic8BallState createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int magicBallValue = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+                child: Image.asset('images/ball$magicBallValue.png'),
+                onPressed: () {
+                  print('The magic 8 ball!');
+                  changeMagicBallValue();
+                },
+              )
+          )
+        ],
+      ),
+    );
+  }
+
+  void changeMagicBallValue() {
+    setState(() {
+      magicBallValue = Random().nextInt(5) + 1;
+    });
+  }
+}
